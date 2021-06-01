54281ba39ebd0567f001a5512902d1630868d5ee
Thomas.D <d.thomas@semifir.com>
2021-06-01 11:11:08 +0200

    Ajout de la logique du Juste prix

--- asset/js/justePrix.js
+++ asset/js/justePrix.js
@@ -9,7 +9,8 @@ let tentatives = 0;
  * Remise à zéro du juste prix
  */
 function reset() {
-    tentative = 0;
+    console.log("(ré)initialisation du Juste Prix");
+    tentatives = 0;
     justePrix = Math.floor(Math.random() * 500);
 }
 
@@ -18,8 +19,30 @@ function reset() {
  */
 function deviner(event){
     event.preventDefault();
-    console.log(nombreInput.value);
-    console.log(nombreInput.value == justePrix);
+    let nbUtilisateur = nombreInput.value;
+    tentatives++;
+    // comparaison du juste prix avec celui indiqué par l'utilisateur
+    if(tentatives < 10){
+
+        if(nbUtilisateur == justePrix){
+            // changement de l'indice
+            indice.innerText = "Vous avez trouver le juste prix !";
+            reset();
+        }else{
+            if(nbUtilisateur > justePrix){
+                // changement de l'indice
+                indice.innerText = `C'est moins ! il reste : ${10 - tentatives} essais`;
+            }else{
+                // changement de l'indice
+                indice.innerText = `C'est plus ! il reste : ${10 - tentatives} essais`;
+            }
+        }
+    }else{
+        indice.innerText = "Vous avez perdu. Le juste prix était : " + justePrix;
+        reset();
+    }
 }
 
 formulaire.addEventListener("submit", deviner);
+// écouter l'événement "load" (chargement) avant de lancer la fonction reset
+window.addEventListener('load',reset);
